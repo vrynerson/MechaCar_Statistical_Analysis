8950d8da710c8ee861bb6fe34b9366fc343f258c
vrynerson <98570777+vrynerson@users.noreply.github.com>
2022-07-10 21:19:40 -0700

    dev 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,7 +4,7 @@
 library(dplyr)
 
 ## read CSV file
-MechaCar_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+MechaCar_data <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
 
 ## linear regression 
 mod <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data)
@@ -12,4 +12,15 @@ mod <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearan
 ## summary of linear regression
 summary(mod)
 
-## p-value: 5.35e-11, Multiple R-squared:  0.7149
+
+## Deliverable 2
+library(tidyverse)
+
+## read CSV file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+
+## summary stats
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+# Manufacturing lot summary stats
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
