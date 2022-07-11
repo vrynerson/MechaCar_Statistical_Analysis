e765528023e3a12de020bf43212d3dff8c5d74ed
vrynerson <98570777+vrynerson@users.noreply.github.com>
2022-07-10 23:33:16 -0700

    dev 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -24,3 +24,18 @@ total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI
 
 # Manufacturing lot summary stats
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+## Deliverable 3
+
+# t-test on population mean of 1500
+t.test(Suspension_Coil$PSI, mu = 1500)
+
+# t-test on each lot
+# lot 1 t-test
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+
+# lot 2 t-test
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+
+# lot 3 t test
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
