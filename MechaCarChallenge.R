88c2b1fc3d50eef793c0cfd3e71951a12ca822b2
vrynerson <98570777+vrynerson@users.noreply.github.com>
2022-07-10 20:52:42 -0700

    dev 1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,13 +1,15 @@
+## Deliverable 1
+
+# load package
 library(dplyr)
 
-## Dev 1
 ## read CSV file
 MechaCar_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 ## linear regression 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data)
+mod <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data)
 
 ## summary of linear regression
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data))
+summary(mod)
 
-## p-value: 5.35e-11, Multiple R-squared:  0.7149, Adjusted R-squared:  0.6825 
+## p-value: 5.35e-11, Multiple R-squared:  0.7149
